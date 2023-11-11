2f7dd01fe751c5d23bc7e70bfc24c3e91ffd8f06
JuscelinoMamede <juscemamede01@gmail.com>
2023-11-10 23:00:06 -0300

    :lipstick:Ajustes finais de style header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./header.css";
 import Button from "../Button/Button";
+import NavBar from "../NavBar/NavBar";
+
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
         <div className="row">
           <img className="w-logo" src="/logo2.png" alt="logo-img" />
-          <div className="col row justify-content-center">
+          <div className="col row justify-content-end align-items-center">
+            <NavBar />
             <Button btnValue="Entrar" />
           </div>
         </div>
