1e42fa00e4a3dc40d5d9a54219f4790bf4818856
JuscelinoMamede <juscemamede01@gmail.com>
2023-11-10 22:06:36 -0300

    Criação do Header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="w-logo" src="/logo2.png" alt="logo-img" /> navbar
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
-import Botao from "./components/Botao";
-import Btn2 from "./components/Btn2";
-import { Header } from "./components/Header";
-import { Imagem } from "./components/Imagem";
-import { ListaItens } from "./components/ListaItens";
-import { Paragrafo } from "./components/Paragrafo";
-import { Quadrado } from "./components/Quadrado";
-import { Rodape } from "./components/Rodape";
-import { Titulo } from "./components/Titulo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header />
-      <Titulo />
-      <Quadrado />
-      <Botao />
-      <Btn2 />
-      <ListaItens />
-      <Imagem />
-      <Paragrafo />
-      <Rodape />
-    </main>
+    </>
   );
 }
